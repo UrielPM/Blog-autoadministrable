13ee4b0e512ee4ac189d7d2a4688cc397e6b679d
Uriel Mora <upm1808@gmail.com>
2021-06-24 13:00:50 -0500

    primer commit

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,31 @@
+@extends('adminlte::page')
+
+@section('title', 'MoraDev')
+
+@section('content_header')
+
+<a class =" btn btn-secondary btn-sm float-right" href="{{route('admin.posts.create')}}">Nuevo Posts</a>
+    <h1>Listado de Posts</h1>
+@stop
+
+@section('content')
+
+@section('content')
+
+    @if (session('info'))
+    <div class="alert alert-success">
+        <strong>{{session('info')}} </strong>
+    </div>
+        
+    @endif
+    
+    @livewire('admin.posts-index')
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
